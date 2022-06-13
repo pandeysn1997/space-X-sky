b5c626094d0a7d3b52fa4b5f7e01ab064077af45
unknown <pandeysn1997@gmail.com>
2022-06-13 12:44:40 +0530

    this is commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,94 @@
 import React, { useEffect, useState } from 'react'
-const App = () => {
-  const [data, setData] = useState([])
+//import './App.css';
 
-  const getData = async () => {
+const App = () => {
+  const [product, setProduct] = useState([])
+
+  const getInfo = async () => {
     const response = await fetch("https://api.spacexdata.com/v4/launches/past");
-    setData(await response.json());
+    setProduct(await response.json());
+
 
   }
   useEffect(() => {
-    getData();
+    getInfo();
 
   }, []);
-  
+
   return (
     <>
       <h1 className="text centre">SpaceX Past Launches </h1>
       <div className="container-fluid mt-5">
         <div className="row text-center">
           {
-            data.map((missions) => {
+
+
+
+
+
+            product.map((missions) => {
               return (
-                
-                <div className="col-12 col-md-4 mt-8">
-                <div className="card" style={{ width: "20rem" }} key = {missions.id}>
-  <img src="https://images2.imgbox.com/00/2f/FhtEd0nB_o.png" className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">{missions.name}</h5>
-    <p className="card-text">
-      {missions.details}
-      <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white status">
-      <div class="d-flex flex-row bg-secondary"><span className="Flight number">Flight No.</span>{missions.flight_number}</div>
-      <div className="d-flex flex-row bg-secondary"><span className="followers">Success</span>{missions.success}</div>
-      <div class="d-flex flex-row bg-secondary"><span className="ratingd">Ratings</span>3.8</div>
-      </div>
-      <p class="card-text"><small class="text-muted"> last updated few mins ago</small></p>
-    </p>
-    {/* <button onClick = {data.map} className="btn btn-primary btn-sm"> */}
-      {/* Get to details</button> */}
-    
-    
-  </div>
-</div>
-</div>
+                <><div class="row row-cols-1 row-cols-md-3 g-4">
+                  <div className="container mt-5">
+                    <div className='col'>
+
+
+                      <div className="card" style={{ width: 400 }} key={missions.id}>
+                        <img class="img-responsive" src="https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png" alt="mission" />
+                        <div className="card-body">
+                          <h4 className="card-title">{missions.name}</h4>
+                          <p className="card-text">
+                            {missions.details}
+                          </p>
+
+                        </div>
+                      </div>
+                    </div>
+                    <br />
+
+
+
+
+                  </div>
+                </div>
+
+                </>
+
 
 
               )
             }
-            )
-          }
+            )}
+
         </div>
+      </div>
+      <div>
+        <div className="container-fluid mt-5">
+
+
+
+        <ul class="pagination pagination-lg">
+            <li>
+              <a href=" ">1</a>
+            </li>
+            <li className="active">
+              <a href=" ">2</a>
+            </li>
+            <li>
+              <a href=" ">3</a>
+            </li>
+            <li>
+              <a href=" ">4</a>
+            </li>
+            <li>
+              <a href=" ">5</a>
+            </li>
+          </ul>
+        </div>
+
+
+
+
       </div>
     </>
   )
